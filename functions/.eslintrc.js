95430ed4adf08ece80311d1a8872d56fbc31c670
anshu-nun <anshucv1@gmail.com>
2025-01-06 10:10:34 -0500

    update eslint

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
-    "no-unused-vars": "off"
+    "no-unused-vars": "off",
+    "no-redeclare": "off"
   },
 };
