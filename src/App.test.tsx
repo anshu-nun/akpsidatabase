ea8e59f54cc9d67a96bca4d5e952d5950c084f28
anshu-nun <anshucv1@gmail.com>
2025-01-06 14:07:22 -0500

    test change

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,9 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', async () => {
+
   const {getByText} = await render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  
 });
